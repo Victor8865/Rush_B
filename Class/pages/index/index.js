54aafa63d60ed39c6e7bb2837bdea64b4773d092
Victor8865 <377502115@qq.com>
2018-04-21 14:53:57 +0800

    WTH
    Change style

--- Class/pages/index/index.js
+++ Class/pages/index/index.js
@@ -6,19 +6,42 @@ Page({
 data : {
 
   day : null,
-  table : null
+  table : "Welcome",
+  items: [
+    {name:'all',value:'All'},
+    { name: 'monday', value: 'Monday' },
+    { name: 'tuesday', value: 'Tuesday' },
+    { name: 'wedsday', value: 'Wedsday' },
+    { name: 'thursday', value: 'Thursday' },
+    { name: 'friday', value: 'Friday' },
+  ]
+
 },
 
- getinfo:function(day)
- {
-   var thispage = this;
-   console.log(this.data.day);
-   app.getClassInfo(this.data.day, function(data){
-     console.log(data);
-     thispage.setData({table : data})
-     
-   });
- },
+
+
+radioChange: function (e) {
+  //console.log('radio发生change事件，携带value值为：', e.detail.value);
+  var thispage = this;
+  console.log(e.detail.value);
+  app.getClassInfo(e.detail.value,function(data){
+    console.log(data);
+    thispage.setData({table : data});
+  })
+},
+
+
+
+//  getinfo:function(day)
+//  {
+//    var thispage = this;
+//    console.log(this.data.day);
+//    app.getClassInfo(this.data.day, function(data){
+//      console.log(data);
+//      thispage.setData({table : data});
+//    });
+   
+//  },
   input:function(e){
     console.log(e)
     this.setData({day : e.detail.value})
